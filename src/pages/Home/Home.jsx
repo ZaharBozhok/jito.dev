fefc8c84215e03c2e6613426bfde194c750f08bc
Kirill <jiecoryi3@gmail.com>
2022-07-24 19:06:29 +0300

    [init] Create simple structure

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+import * as Styled from './styles';
+
+export default function Home() {
+    return (
+        <Styled.Home>
+            
+        </Styled.Home>
+    )
+}
